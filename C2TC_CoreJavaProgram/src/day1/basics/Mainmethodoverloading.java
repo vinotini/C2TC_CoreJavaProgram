e269e266176d0f6c7770b02c2ed104dad2fe847d
STUDENT <STUDENT@DESKTOP-JRMPI7J>
2025-07-21 02:27:37 -0700

    these are my programs.

---
+++ C2TC_CoreJavaProgram/src/day1/basics/Mainmethodoverloading.java
@@ -0,0 +1,19 @@
+package day1.basics;
+
+public class Mainmethodoverloading {
+	public static void main() {
+		System.out.println("Welcom form main 1");
+	}
+	
+	public static void main(char c[])
+	{
+		System.out.println("Welcome form main 2");
+	}	
+    public static void main(String[] args)
+    {
+    	System.out.println("Welcome form main 3");
+		
+
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/day1/basics/additindemo.java
@@ -0,0 +1,20 @@
+package day1.basics;
+
+import java.util.Scanner;
+
+public class additindemo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is "+c);
+      
+				
+
+	}
+
+}
