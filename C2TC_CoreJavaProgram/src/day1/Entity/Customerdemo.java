2b68a28e04e73758d7c8611a6906f3895691df8a
STUDENT <STUDENT@DESKTOP-JRMPI7J>
2025-07-21 03:43:37 -0700

    customer programs

---
+++ C2TC_CoreJavaProgram/src/day1/Entity/Customerdemo.java
@@ -0,0 +1,21 @@
+package day1.Entity;
+
+public class Customerdemo {
+
+	public static void main(String[] args) {
+		Customerjava c1 = new Customerjava();
+		c1.setCid(101);
+		c1.setCname("raja");
+		c1.setCity("puducherry");
+		System.out.println(c1);
+		
+		Customerjava c2 = new Customerjava();
+		c2.setCid(102);
+		c2.setCname("vin");
+		c2.setCity("puducherry");
+		System.out.println(c2);
+		
+		
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/day1/Entity/Customerjava.java
@@ -0,0 +1,31 @@
+package day1.Entity;
+
+public class Customerjava {
+
+
+		private int cid;
+		private String cname;
+		private String city;
+		
+		public int getCid() {
+			return cid;
+		}
+		public void setCid(int cid) {
+			this.cid = cid;
+		}
+		public String getCname() {
+			return cname;
+		}
+		public void setCname(String cname) {
+			this.cname = cname;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		
+		
+
+}
