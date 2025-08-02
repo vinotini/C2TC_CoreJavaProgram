0ca40aa88f1ff1d4c77a8e3c58c86985ac56d6cb
Dell <Dell@vin.lan>
2025-08-02 08:59:12 +0530

    day 13 programs

---
+++ C2TC_CoreJavaProgram/src/DayThirteen/ThreadLifeCycle/MyChildThread.java
@@ -0,0 +1,21 @@
+package DayThirteen.ThreadLifeCycle;
+
+//Program to define ChildThread 
+
+public class MyChildThread extends Thread {
+	@Override
+	public void run() {
+		System.out.println("In side run() Thread is alive or not? " + this.isAlive());
+		int no = 0;
+		while (no < 4) {
+			no++;
+			System.out.println("number = " + no);
+			try {
+				sleep(500);
+
+			} catch (InterruptedException exp) {
+				System.err.println("Thread Interrupted ...");
+			}
+		}
+	}
+}
---
+++ C2TC_CoreJavaProgram/src/DayThirteen/ThreadLifeCycle/ThreadLifeCycleDemo.java
@@ -0,0 +1,20 @@
+package DayThirteen.ThreadLifeCycle;
+
+//Program to demonstrate Thread life cycle
+
+public class ThreadLifeCycleDemo {
+
+	public static void main(String[] args) {
+		MyChildThread myThread = new MyChildThread();
+		System.out.println("Before Runnable stage Thread is alive or not? " + myThread.isAlive());
+		myThread.start();
+
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException exp) {
+			System.err.println("Thread is interrupted !");
+		}
+
+		System.out.println("After complete execution of Thread ,it is alive or not? " + myThread.isAlive());
+	}
+}
